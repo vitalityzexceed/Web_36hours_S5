22a21e634edef7be32cd022210ebd6b14ee7ce4c
vitalityzexceed <greatotakuofrog@gmail.com>
2023-07-10 17:45:27 +0300

    ajout nouveaux modeles

---
+++ application/models/Model_element.php
@@ -0,0 +1,46 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Model_element extends CI_Model
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('model_generalise');
+    }
+
+    public function delete_element($idelement)
+    {
+        $this->db->where('$id_element', $idelement);
+        $this->db->delete('element');
+
+        $this->db->where('$id_element', $idelement);
+        $this->db->delete('regime_element');
+        
+    }
+
+    public function update_element($idelement, $nom, $prix)
+    {
+        $new_data = [
+            'nom' => $nom,
+            'prix' => $prix,
+        ];
+
+        $this->db->where('$id_element', $idelement);
+        $this->db->update('element', $new_data);
+    }
+
+    public function insert_new_element($nom, $prix)
+    {
+        $data = [
+            'nom' => $nom,
+            'prix' => $prix,
+        ];
+        $this->db->insert('element', $data);
+    }
+
+    
+}
+
+?>
